96bfa8aa327dcf5bd780b42226ce0c4d9d4c32c6
gov <govi218mu@gmail.com>
2018-07-16 18:15:56 -0400

    user-generated

--- src/com/coinroster/api/SettleContest.java
+++ src/com/coinroster/api/SettleContest.java
@@ -58,7 +58,7 @@ public class SettleContest extends Utils
 			// lock it all
 			
 			Statement statement = sql_connection.createStatement();
-			statement.execute("lock tables user write, contest write, entry write, transaction write, progressive write, voting write");
+			statement.execute("lock tables user write, contest write, entry write, transaction write, progressive write, voting write, control write");
 
 			try {
 				lock : {
