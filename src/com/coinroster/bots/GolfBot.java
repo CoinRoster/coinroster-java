6ae2f97ffd30309d977946cde956576a37113e21
Cole Fisher <cfishe29@uwo.ca>
2018-07-21 21:25:21 -0400

    replace inactives functionality

--- src/com/coinroster/bots/GolfBot.java
+++ src/com/coinroster/bots/GolfBot.java
@@ -1011,6 +1011,23 @@ public class GolfBot extends Utils {
 		}
 	}
 	
+	public JSONObject replaceInactive(String id, double salary) throws SQLException, JSONException{
+		JSONObject player = new JSONObject();
+		PreparedStatement get_player = sql_connection.prepareStatement("SELECT id, name, salary from player where id != ? "
+																	+ "and sport_type = \"GOLF\" and filter_on = 4 and salary <= ? "
+																	+ "order by salary desc, name asc limit 1");
+		get_player.setString(1,  id);
+		get_player.setDouble(2, salary);
+		ResultSet rtn = get_player.executeQuery();
+		if(rtn.next()){
+			player.put("id", rtn.getString(1));
+			player.put("name", rtn.getString(2));
+			player.put("price", rtn.getDouble(3));
+		}
+		return player;
+	}
+	
+	
 	class Player {
 		public String method_level = "admin";
 		private String name;
