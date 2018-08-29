e77d59e927c0023740ee5242006e6889ee1a5a02
Cole Fisher <cfishe29@uwo.ca>
2018-08-29 13:35:02 -0400

    refactoring basketball bot to exclude suspended games

--- src/com/coinroster/bots/BasketballBot.java
+++ src/com/coinroster/bots/BasketballBot.java
@@ -71,8 +71,18 @@ public class BasketballBot extends Utils {
 			return null;
 		}
 		else{
-			String earliest_date = events.getJSONObject(0).getString("date");
-	        
+			boolean ready = false;
+			int index = 0;
+			String earliest_date = null;
+			while(!ready){
+				
+				earliest_date = events.getJSONObject(index).getString("date");
+				// check if suspended
+				if(events.getJSONObject(index).getJSONObject("status").getJSONObject("type").getString("name").equals("STATUS_SUSPENDED"))
+					index++;
+				else
+					ready = true;
+	        }
 	        try {
 	            Date date = formatter1.parse(earliest_date.replaceAll("Z$", "+0000"));
 	            long milli = date.getTime();
@@ -82,7 +92,7 @@ public class BasketballBot extends Utils {
 	            e.printStackTrace();
 	        }
 
-			for(int i=0; i < events.length(); i++){
+			for(int i=index; i < events.length(); i++){
 				JSONObject game = events.getJSONObject(i);
 				String name = game.getString("shortName");
 				Date game_date = formatter1.parse(game.getString("date").replaceAll("Z$", "+0000"));
@@ -454,10 +464,10 @@ public class BasketballBot extends Utils {
 			}
 			log("valid?" + sql_connection.isValid(0));
 			
-			PreparedStatement delete_old_rows = sql_connection.prepareStatement("delete from player where sport_type=?");
-			delete_old_rows.setString(1, this.sport);
-			delete_old_rows.executeUpdate();
-			log("deleted " + this.sport + " players from old contests");
+//			PreparedStatement delete_old_rows = sql_connection.prepareStatement("delete from player where sport_type=?");
+//			delete_old_rows.setString(1, this.sport);
+//			delete_old_rows.executeUpdate();
+//			log("deleted " + this.sport + " players from old contests");
 			
 			JSONObject empty_data_json = new JSONObject();
 			empty_data_json.put("fgm", 0);
